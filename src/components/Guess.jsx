6a06da2e89fe1a3b36764bb592b96fc7dc1741b3
Laura <lbrown@guidespark.com>
2022-04-20 16:29:28 -0700

    working on color contrast

--- src/components/Guess.jsx
+++ src/components/Guess.jsx
@@ -5,26 +5,105 @@ export default function Guess() {
   const [rVal, setRVal] = useState("");
   const [gVal, setGVal] = useState("");
   const [bVal, setBVal] = useState("");
+  const [rgb, setRgb] = useState("rgba(255, 255, 255, 1)");
+  const [contrast, setContrast] = useState("");
 
   useEffect(() => {
-    console.log(rVal);
-  }, [rVal]);
+    if (rVal < 0) {
+      setRVal(0);
+    }
+    if (rVal > 255) {
+      setRVal(255);
+    }
+    if (gVal < 0) {
+      setGVal(0);
+    }
+    if (gVal > 255) {
+      setGVal(255);
+    }
+    if (bVal < 0) {
+      setBVal(0);
+    }
+    if (bVal > 255) {
+      setBVal(255);
+    }
+  }, [rVal, gVal, bVal]);
+
+  const [disableInputs, setDisableInputs] = useState(false);
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    setRgb(`rgba(${rVal}, ${gVal}, ${bVal}, 1)`);
+    setDisableInputs(true);
+    calculateContrast();
+  };
+
+  const calculateContrast = () => {
+    //from here: https://stackoverflow.com/questions/3942878/how-to-decide-font-color-in-white-or-black-depending-on-background-color
+    const rgbArr = [rVal, bVal, gVal];
+    const adjArr = [];
+    rgbArr.forEach((color) => {
+      color = color / 255;
+      if (color <= 0.03928) {
+        color = color / 12.92;
+      } else {
+        color = ((color + 0.055) / 1.055) ^ 2.4;
+      }
+      adjArr.push(color);
+    });
+    let luminance =
+      0.2126 * adjArr[0] + 0.7152 * adjArr[1] + 0.0722 * adjArr[2];
+    if (luminance > 0.179) {
+      setContrast("black");
+    } else {
+      setContrast("white");
+    }
+    // if L > 0.179 use #000000 else use #ffffff
+    //     for each c in r,g,b:
+    //     c = c / 255.0
+    //     if c <= 0.03928 then c = c/12.92 else c = ((c+0.055)/1.055) ^ 2.4
+    // L = 0.2126 * r + 0.7152 * g + 0.0722 * b
+  };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-around",
-        alignItems: "center",
-        width: 384,
-        height: 70,
-        border: "3px solid #CDD0D5",
-        margin: "6px 0",
-      }}
-    >
-      <GuessComp letter={"R"} val={rVal} passVal={setRVal} />
-      <GuessComp letter={"G"} val={""} />
-      <GuessComp letter={"B"} val={""} />
-    </div>
+    <form onSubmit={onFormSubmit}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+          width: 384,
+          height: 70,
+          border: "3px solid #CDD0D5",
+          margin: "6px 0",
+          backgroundColor: rgb,
+        }}
+      >
+        <GuessComp
+          letter={"R"}
+          val={rVal}
+          passVal={setRVal}
+          disable={disableInputs}
+          bw={contrast}
+        />
+        <GuessComp
+          letter={"G"}
+          val={gVal}
+          passVal={setGVal}
+          disable={disableInputs}
+          bw={contrast}
+        />
+        <GuessComp
+          letter={"B"}
+          val={bVal}
+          passVal={setBVal}
+          disable={disableInputs}
+          bw={contrast}
+        />
+        <button type="submit" style={{ display: "none" }}>
+          Submit
+        </button>
+      </div>
+    </form>
   );
 }
--- src/components/GuessComp.jsx
+++ src/components/GuessComp.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TextField, InputAdornment } from "@mui/material/";
 
 export default function GuessComp(props) {
+  const [labelColor, setLabelColor] = useState("#929292");
+  const [inputColor, setInputColor] = useState("#2C2C2C");
+
+  useEffect(() => {
+    if (props.bw !== "") {
+      setLabelColor(props.bw);
+      setInputColor(props.bw);
+    }
+  }, [props.bw]);
+
   return (
     <TextField
       id={`${props.letter}-value`}
       InputProps={{
         type: "number",
-        style: { fontSize: 40, color: "#2C2C2C", height: 40, margin: "0 5px" },
+        style: {
+          fontSize: 40,
+          color: inputColor,
+          height: 40,
+          margin: "0 5px",
+          "&$disabled": {
+            borderColor: "orange",
+          },
+        },
         startAdornment: (
           <InputAdornment position="start">
-            <p style={{ fontSize: 25, color: "#929292" }}>{props.letter}</p>
+            <p style={{ fontSize: 25, color: labelColor }}>{props.letter}</p>
           </InputAdornment>
         ),
       }}
@@ -18,6 +36,7 @@ export default function GuessComp(props) {
       variant="standard"
       value={props.val}
       onChange={(e) => props.passVal(e.target.value)}
+      disabled={props.disable}
     />
   );
 }
