fbbb282623dbb86c3801464d81e37b2f4b8dd09c
Laura <lbrown@guidespark.com>
2022-04-29 16:27:26 -0700

    popover for copied content

--- src/components/ShareBtn.jsx
+++ src/components/ShareBtn.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { Button } from "@mui/material";
 import { TheDay } from "./TheColor";
+import { Popover, Typography } from "@mui/material";
 
 export default function ShareBtn(props) {
+  //popover stuff
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
+  //day number
   const dayNo = TheDay._currentValue;
-  const copyAns = () => {
+
+  //clipboard stuff
+  const copyAns = (e) => {
     const finalArr = props.final;
     const stringArr = [];
     stringArr.push(`RGBdle ${dayNo}: ${props.final.length}/6 \n`);
@@ -14,26 +28,47 @@ export default function ShareBtn(props) {
     });
     stringArr.push("\n[url here]");
     const finalStr = stringArr.join("\n");
-    copyToClipBoard(finalStr);
+    copyToClipBoard(finalStr, e);
   };
 
   //from here: https://www.delftstack.com/howto/javascript/javascript-copy-to-clipboard/
-  function copyToClipBoard(content) {
-    navigator.clipboard.writeText(content).catch((err) => {
-      console.log("Something went wrong", err);
-    });
+  function copyToClipBoard(content, event) {
+    navigator.clipboard
+      .writeText(content)
+      .then(setAnchorEl(event.currentTarget))
+      .catch((err) => {
+        console.log("Something went wrong", err);
+      });
   }
 
   if (props.final.length > 0) {
     return (
-      <Button
-        variant="contained"
-        color="grey"
-        style={{ marginBottom: "1rem" }}
-        onClick={copyAns}
-      >
-        Share
-      </Button>
+      <div>
+        <Button
+          variant="contained"
+          color="grey"
+          style={{ marginBottom: "1rem" }}
+          onClick={(e) => copyAns(e)}
+        >
+          Share
+        </Button>
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center",
+          }}
+        >
+          <Typography sx={{ p: 2 }}>Content copied to clipboard.</Typography>
+        </Popover>
+      </div>
     );
   } else {
     return (
